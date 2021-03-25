69b066422697bc47dbabfa790a2a863a17f45410
Chiranjeev Das <chiranjeevdas1996@gmail.com>
2021-03-25 16:14:21 +0530

    substituted cran and bioconductor packages for apt

--- etenbr_installer.bash
+++ etenbr_installer.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # rna seq pipeline prerequisites install script (bash)
+# this script has been tested to work with Ubuntu 20.04.2 LTS
+# other OS versions may require manual installation of the applications
 
 echo "NOTE: root access is necessary for the script"
 echo "NOTE: for cluster environment, the script must be run on each node"
@@ -53,154 +55,46 @@ echo [`date +"%Y-%m-%d %H:%M:%S"`] "sra-toolkit installation complete"
 echo "##############################################################"
 
 echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing pigz now"
+echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing apt packages now"
 echo "##############################################################"
 
 sudo apt install pigz
 sleep 05
-
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "pigz installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing fastp now"
-echo "##############################################################"
-
 sudo apt install fastp
 sleep 05
-
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "fastp installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing fastqc now"
-echo "##############################################################"
-
 sudo apt install fastqc
 sleep 05
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "fastqc installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing hisat2 now"
-echo "##############################################################"
-
 sudo apt install hisat2
 sleep 05
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "  installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing samtools now"
-echo "##############################################################"
-
 sudo apt install samtools
 sleep 05
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "samtools installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing stringtie now"
-echo "##############################################################"
-
 sudo apt install stringtie
 sleep 05
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "stringtie installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing featurecounts (subread) now"
-echo "##############################################################"
-
 sudo apt install subread
 sleep 05
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "subread installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "installing R now"
-echo "##############################################################"
-
 sudo apt install r-base
 sleep 05
-
 sudo apt install littler
 sleep 05
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "R installation complete"
-echo "##############################################################"
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "creating R script for R packages installation"
-echo "##############################################################"
-
-cat > etenbr_installer.r<< EOF
-#!/usr/bin/r
-
-##	IF YOUR SYSTEM IS BEHIND A PROXY, MODIFY THE FOLLOWING AND UNCOMMENT THEM
-#	Sys.setenv(http_proxy="http://user:passwd@url:port")
-#	Sys.setenv(http_proxy="https://user:passwd@url:port")
-
-update.packages(ask = FALSE)
-
-install.packages("readr")
-
-install.packages("stringr")
-
-install.packages("ggplot2")
-
-install.packages("dplyr")
-
-install.packages("ashr")
-
-install.packages("gplots")
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-library("BiocManager")
-
-BiocManager::install("DESeq2")
-
-BiocManager::install("tximport")
-
-BiocManager::install("apeglm")
-
-BiocManager::install("vsn")
-
-EOF
-
+sudo apt install r-cran-biocmanager
+sleep 05
+sudo apt install r-cran-stringr
+sleep 05
+sudo apt install r-bioc-deseq2
+sleep 05
+sudo apt install r-cran-ggplot2
+sleep 05
+sudo apt install r-cran-dplyr
+sleep 05
+sudo apt install r-cran-readr
+sleep 05
+sudo apt install r-bioc-tximport
+sleep 05
+subo apt install r-cran-gplots
 sleep 05
 
 echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "R script created and written to etenbr_installer.R"
-echo "##############################################################"
-
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "executing R script for package installation"
-echo "##############################################################"
-
-sudo r etenbr_installer.r
-sleep 05
-
-echo "##############################################################"
-echo [`date +"%Y-%m-%d %H:%M:%S"`] "R packages installation complete"
+echo [`date +"%Y-%m-%d %H:%M:%S"`] "apt packages installation complete"
 echo "##############################################################"
 
 echo "##############################################################"
